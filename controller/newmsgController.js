1782633e74d649a3f674db1b5c273558a4676c6f
AustinDsz <austindsz21@gmail.com>
2024-09-06 16:03:18 +0530

    updates to css

--- controller/newmsgController.js
+++ controller/newmsgController.js
@@ -3,7 +3,9 @@ const asyncHandler = require("express-async-handler");
 const messages = require("../data/messages");
 const date = new Date();
 const month = date.toLocaleString('default', { month: 'long' });
-
+let hours = date.getHours();
+let newformat = hours >=12 ? 'PM': 'AM';
+hours = hours % 12;
 
 
 
@@ -12,8 +14,11 @@ const getForm = asyncHandler(async (req, res)=>{
 });
 
 const postMessagefromForm = asyncHandler(async(req, res)=>{
+    
 
-    messages.push({text: req.body.messageText, user: req.body.userName, added:`${date.getDate()} ${month} ${date.getFullYear()}`});
+
+
+    messages.push({text: req.body.messageText, user: req.body.userName, added:`${date.getDate()} ${month} ${date.getFullYear()} ${hours}: ${date.getMinutes()} ${newformat}`});
     res.redirect("..");
 
 });
--- data/messages.js
+++ data/messages.js
@@ -1,5 +1,8 @@
 const date = new Date();
 const month = date.toLocaleString('default', { month: 'long' });
+let hours = date.getHours();
+let newformat = hours >=12 ? 'PM': 'AM';
+hours = hours % 12;
 
 
 //messages
@@ -7,12 +10,12 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: `${date.getDate()} ${month} ${date.getFullYear()}`
+    added: `${date.getDate()} ${month} ${date.getFullYear()} ${hours}: ${date.getMinutes()} ${newformat}`
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: `${date.getDate()} ${month} ${date.getFullYear()}`
+    added: `${date.getDate()} ${month} ${date.getFullYear()} ${hours}: ${date.getMinutes()} ${newformat}`
   }
 ];
 
