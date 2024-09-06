aabed92d26204158d4093b5d8add882fff7364c2
AustinDsz <austindsz21@gmail.com>
2024-09-06 20:58:38 +0530

    modified app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const app = express();
+const app = express()
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const path = require("node:path");
 const indexRouter = require("./router/indexRouter");
